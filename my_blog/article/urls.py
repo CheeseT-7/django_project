f4a44df67881a06ad58c99437825b4da29c5d6b1
cheese <470187906@qq.com>
2021-12-19 12:54:26 +0800

    first Django project with article app.

---
+++ my_blog/article/urls.py
@@ -0,0 +1,19 @@
+#!/user/bin/env python
+# coding=utf-8
+"""
+@project : django_project
+@author  : CheeseT
+#@file   : urls.py
+#@ide    : PyCharm
+#@time   : 2021-12-19 12:36:36
+"""
+
+# 引入path
+from django.urls import path
+
+# 正在部署的应用app的名称
+app_name = 'article'
+
+urlpatterns = [
+    # 目前还没有urls
+]
