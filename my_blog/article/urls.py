08627abf84e3c69eff566e03c3fc0e07b969b603
cheese <470187906@qq.com>
2021-12-22 02:57:00 +0800

    modified view content

--- my_blog/article/urls.py
+++ my_blog/article/urls.py
@@ -18,5 +18,5 @@ app_name = 'article'
 
 urlpatterns = [
     # path函数将url映射到视图
-    path('article_list/', views.article_list, name='article_list'),
+    path('article-list/', views.article_list, name='article_list'),
 ]
--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 # 导入HttpResponse模块
 from django.http import HttpResponse
+# 导入数据模型ArticlePost
+from .models import ArticlePost
+
 # Create your views here.
 
 
 # 视图函数
 def article_list(request):
-    return HttpResponse("Hello World!")
+    # 取出所有博客文章
+    articles = ArticlePost.objects.all()
+    # 需要传递给模版（templates）的对象
+    context = {'articles': articles}
+    # render函数：载入模版，并返回context对象
+    return render(request, 'article/list.html', context)
+
+
