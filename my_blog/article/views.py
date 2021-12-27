0a6b591d822f1cbb9baf9a107a93c9433aeb16ac
cheese <470187906@qq.com>
2021-12-27 17:41:19 +0800

    add article detail page

--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -17,3 +17,11 @@ def article_list(request):
     return render(request, 'article/list.html', context)
 
 
+# 文章详情
+def article_detail(request, id):
+    # 取出相应的文章
+    article = ArticlePost.objects.get(id=id)
+    # 需要传递给模版的对象
+    context = {'article': article}
+    # 载入模版，并返回context对象
+    return render(request, 'article/detail.html', context)
