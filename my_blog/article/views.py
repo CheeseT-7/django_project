84aa8e6ce2e61f2fc30bd96e14a60353300b2364
cheese <470187906@qq.com>
2022-01-04 16:32:47 +0800

    add article safe delete function

--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -75,3 +75,22 @@ def article_create(request):
         # 返回模版
         return render(request, 'article/create.html', context)
 
+
+# 删除文章
+def article_delete(request, id):
+    # 根据id获取需要删除的文章
+    article = ArticlePost.objects.get(id=id)
+    # 调用delete()方法删除文章
+    article.delete()
+    # 完成删除后返回文章列表
+    return redirect('article:article_list')
+
+
+# 安全删除文章
+def article_safe_delete(request, id):
+    if request.method == 'POST':
+        article = ArticlePost.objects.get(id=id)
+        article.delete()
+        return redirect('article:article_list')
+    else:
+        return HttpResponse('仅允许post请求')
